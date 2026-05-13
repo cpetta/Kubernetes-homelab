cc1c6f195ca6ea3b0876f53bb1b720025d51d4d4
Chris Petta <cpetta@pm.me>
2026-05-13 12:51:18 -0400

    added machine specific configs for metal control planes

---
+++ terraform/041-talos_metal_control_patch.tf
@@ -0,0 +1,40 @@
+#-------------------------------------------------------
+# Talos Control Plain Image Patch
+#-------------------------------------------------------
+locals {
+  talos_metal_control_patch = {
+    for i, each in var.k8_metal_control_list : i => {
+      machine = {
+        install = {
+          disk  = "/dev/sda"
+          image = data.talos_image_factory_urls.this.urls.installer
+        }
+        network = {
+          interfaces = [
+            {
+              interface = "eth0"
+              dhcp      = false
+
+              # HA Layer 2 VIP configuration
+              vip = {
+                ip = "192.168.0.227"
+              }
+            }
+          ]
+        }
+        kubelet = {
+          extraArgs = {
+            rotate-server-certificates = true
+          }
+        }
+      }
+      cluster = {
+        etcd = {
+          extraArgs = {
+            listen-metrics-urls = "http://0.0.0.0:2381"
+          }
+        }
+      }
+    }
+  }
+}
