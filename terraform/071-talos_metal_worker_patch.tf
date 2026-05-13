72ee2074dd09ac1d0bbf96e8c52ed9ea7cdab31a
Chris Petta <cpetta@pm.me>
2026-05-13 11:04:51 -0400

    add per node config patches

--- terraform/071-talos_metal_worker_patch.tf
+++ terraform/071-talos_metal_worker_patch.tf
@@ -3,50 +3,55 @@
 #-------------------------------------------------------
 locals {
   talos_metal_worker_patch = {
-    machine = {
-      install = {
-        disk       = "/dev/nvme0n1"
-        image      = data.talos_image_factory_urls.metal_worker.urls.installer_secureboot
-        bootloader = true
-        wipe       = true
-      }
-      disks = [
-        {
-          device = "/dev/sda"
-          partitions = [
+    for i, each in var.k8_metal_worker_list : i => {
+      machine = {
+        install = {
+          disk       = "/dev/nvme0n1"
+          image      = data.talos_image_factory_urls.metal_worker.urls.installer_secureboot
+          bootloader = true
+          wipe       = true
+        }
+        network = {
+          hostname = each.hostname
+        }
+        disks = [
+          {
+            device = "/dev/sda"
+            partitions = [
+              {
+                mountpoint = "/var/lib/longhorn"
+                size       = 0
+              }
+            ]
+          },
+        ]
+        kubelet = {
+          extraMounts = [
             {
-              mountpoint = "/var/lib/longhorn"
-              size       = 0
+              destination = "/var/lib/longhorn"
+              type        = "bind"
+              source      = "/var/lib/longhorn"
+              options = [
+                "bind",
+                "rshared",
+                "rw",
+              ]
             }
           ]
-        },
-      ]
-      kubelet = {
-        extraMounts = [
-          {
-            destination = "/var/lib/longhorn"
-            type        = "bind"
-            source      = "/var/lib/longhorn"
-            options = [
-              "bind",
-              "rshared",
-              "rw",
-            ]
+          extraArgs = {
+            rotate-server-certificates = true
           }
-        ]
-        extraArgs = {
-          rotate-server-certificates = true
         }
-      }
-      sysctls = {
-        "vm.nr_hugepages" = "1024"
-      }
-      kernel = {
-        modules = [
-          { name = "nvme_tcp" },
-          { name = "vfio_pci" },
-          { name = "nfsd" }
-        ]
+        sysctls = {
+          "vm.nr_hugepages" = "1024"
+        }
+        kernel = {
+          modules = [
+            { name = "nvme_tcp" },
+            { name = "vfio_pci" },
+            { name = "nfsd" }
+          ]
+        }
       }
     }
   }
--- terraform/081-talos_metal_worker_nodes.tf
+++ terraform/081-talos_metal_worker_nodes.tf
@@ -2,21 +2,23 @@
 # Talos Metal Nodes
 #-------------------------------------------------------
 data "talos_machine_configuration" "metal_worker" {
+  for_each = var.k8_metal_worker_list
   cluster_name       = local.k8_cluster_config.name
   machine_type       = "worker"
   cluster_endpoint   = local.k8_cluster_config.endpoint
   machine_secrets    = talos_machine_secrets.this.machine_secrets
   kubernetes_version = local.k8_cluster_config.kubernetes_version
-  config_patches     = [yamlencode(local.talos_metal_worker_patch)]
+  config_patches     = [yamlencode(local.talos_metal_worker_patch[each.key])]
 }
 
 data "talos_client_configuration" "metal_worker" {
   cluster_name         = local.k8_cluster_config.name
   client_configuration = talos_machine_secrets.this.client_configuration
-  endpoints            = [var.k8_bare_metal_node_list[0].ip_address]
+  endpoints            = [ for node in var.k8_metal_worker_list : node.ip_address ]
 }
 
 resource "local_file" "metal_worker_machine_config" {
-  content  = data.talos_machine_configuration.metal_worker.machine_configuration
-  filename = "${path.module}/../backups/talos/metal_worker_machine_config.yaml"
+  for_each = var.k8_metal_worker_list
+  content  = data.talos_machine_configuration.metal_worker[each.key].machine_configuration
+  filename = "${path.module}/../backups/talos/metal_worker_machine_config_${each.key}.yaml"
 }
