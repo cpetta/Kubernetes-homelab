311decea5a6a4733f32147dd5d024c36de3526d1
Chloe Graves <cpetta@pm.me>
2026-07-08 06:50:21 -0400

    ArgoCD - initial install

---
+++ terraform/121-kubernetes_argo-cd.tf
@@ -0,0 +1,50 @@
+#-------------------------------------------------------
+# ArgoCD
+#-------------------------------------------------------
+resource "kubernetes_namespace_v1" "argo" {
+  metadata {
+    name = "argocd"
+    labels = {}
+  }
+}
+
+#-------------------------------------------------------
+# ArgoCD - DNS
+#-------------------------------------------------------
+resource "dns_a_record_set" "argocd" {
+  zone     = "${var.dns_zone}."
+  name     = "argocd"
+  addresses = [
+    var.k8_service_list.rp,
+  ]
+}
+
+
+#-------------------------------------------------------
+# ArgoCD - Helm & Config
+#-------------------------------------------------------
+resource "local_file" "argo_cd_values" {
+  content = templatefile("${path.module}/helm/templates/argocd.tftpl", {
+    dns_zone = var.dns_zone,
+    subnet = "argocd",
+    server_replicas = 1,
+    oidc_client_id = "",
+    oidc_client_secret = "",
+  })
+  filename = "${path.module}/helm/tmp/argo_cd.yaml"
+}
+
+resource "helm_release" "argocd" {
+  depends_on        = [
+  ]
+  name              = "argocd"
+  namespace         = kubernetes_namespace_v1.argo.id
+  create_namespace  = false
+  repository        = "https://argoproj.github.io/argo-helm"
+  chart             = "argo-cd"
+  version           = "10.1.2"
+
+  values = [
+    local_file.argo_cd_values.content
+  ]
+}
