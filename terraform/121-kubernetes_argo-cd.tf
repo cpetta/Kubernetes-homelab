b5983bd8bb49a3fd3437ea78e8a9b4e6dcdcc54f
Chloe Graves <cpetta@pm.me>
2026-07-11 09:20:26 -0400

    Add ArgoCD self management

--- terraform/121-kubernetes_argo-cd.tf
+++ terraform/121-kubernetes_argo-cd.tf
@@ -39,7 +39,7 @@ resource "kubernetes_secret_v1" "argo_private_repo" {
 }
 
 #-------------------------------------------------------
-# ArgoCD - Helm & Config
+# ArgoCD - Config
 #-------------------------------------------------------
 resource "local_file" "argo_cd_values" {
   content = templatefile("${path.module}/helm/templates/argocd.tftpl", {
@@ -54,17 +54,72 @@ resource "local_file" "argo_cd_values" {
   filename = "${path.module}/helm/tmp/argo_cd.yaml"
 }
 
-resource "helm_release" "argocd" {
-  depends_on        = [
-  ]
-  name              = "argocd"
-  namespace         = kubernetes_namespace_v1.argo.id
-  create_namespace  = false
-  repository        = "https://argoproj.github.io/argo-helm"
-  chart             = "argo-cd"
-  version           = "10.1.2"
+#-------------------------------------------------------
+# ArgoCD - Helm Initial install (cluster without argo)
+# After install and running execute the following command
+# terraform state rm helm_release.argocd
+# then comment out the helm_release.argocd resource below
+#-------------------------------------------------------
+# resource "helm_release" "argocd" {
+#   depends_on        = [
+#   ]
+#   name              = "argocd"
+#   namespace         = kubernetes_namespace_v1.argo.id
+#   create_namespace  = false
+#   repository        = "https://argoproj.github.io/argo-helm"
+#   chart             = "argo-cd"
+#   version           = "10.1.2"
 
-  values = [
-    local_file.argo_cd_values.content
-  ]
+#   values = [
+#     local_file.argo_cd_values.content
+#   ]
+# }
+
+#-------------------------------------------------------
+# Adopt into ArgoCD
+#-------------------------------------------------------
+resource "argocd_application" "argocd" {
+  metadata {
+    name      = "argocd"
+    namespace = kubernetes_namespace_v1.argo.id
+  }
+
+  spec {
+    source {
+      repo_url = "https://argoproj.github.io/argo-helm"
+      chart = "argo-cd"
+      target_revision = "10.1.2"
+      
+      helm {
+        release_name = "argocd"
+        values = local_file.argo_cd_values.content # TODO replace with GitOPs
+      }
+    }
+    
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = kubernetes_namespace_v1.argo.id
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = true
+      }
+      sync_options = [
+        "ServerSideApply=true",
+        "Validate=false",
+      ]
+      
+      retry {
+        limit = "3"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
 }
