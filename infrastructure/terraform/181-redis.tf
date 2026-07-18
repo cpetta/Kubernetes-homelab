7d0c954a2a1b1614e1e06ca22b1626e18b4b2c19
Chloe Graves <cpetta@pm.me>
2026-07-18 09:27:50 -0400

    Migrate redis to ArgoCD

--- infrastructure/terraform/181-redis.tf
+++ infrastructure/terraform/181-redis.tf
@@ -28,51 +28,6 @@ resource "kubernetes_namespace_v1" "redis" {
 }
 
 #-------------------------------------------------------
-# Redis - Secrets
-#-------------------------------------------------------
-resource "kubernetes_secret_v1" "redis_password" {
-  metadata {
-    name      = "redis-password"
-    namespace = kubernetes_namespace_v1.redis.id
-  }
-
-  data = {
-    password       = var.redis_password
-    redis-password = var.redis_password
-  }
-
-  type = "Opaque"
-}
-
-resource "kubernetes_secret_v1" "redis_password_nextcloud" {
-  metadata {
-    name      = "redis-config"
-    namespace = kubernetes_namespace_v1.nextcloud.id
-  }
-
-  data = {
-    host           = "redis.redis.svc.cluster.local"
-    redis-password = var.redis_password
-  }
-
-  type = "Opaque"
-}
-
-resource "kubernetes_secret_v1" "redis_secrets_oauth2proxy" {
-  metadata {
-    name      = "redis-config"
-    namespace = "traefik"
-  }
-
-  data = {
-    host           = "redis.redis.svc.cluster.local"
-    redis-password = var.redis_password
-  }
-
-  type = "Opaque"
-}
-
-#-------------------------------------------------------
 # Redis - Storage
 #-------------------------------------------------------
 resource "kubernetes_manifest" "redis_longhorn_volume" {
@@ -167,4 +122,57 @@ resource "helm_release" "redis" {
         pvc_replica = "${local.redis.volumes.replica.name}-pvc"
     })))
   ]
+}
+
+resource "argocd_application" "redis" {
+  metadata {
+    name      = "redis"
+    namespace = kubernetes_namespace_v1.argo.id
+  }
+
+  spec {
+    source {
+      repo_url = "https://charts.bitnami.com/bitnami"
+      chart = "redis"
+      target_revision = "27.0.10"
+      
+      helm {
+        release_name = "redis"
+        value_files = ["$config/applications/redis/values.yaml"]
+      }
+    }
+
+    source {
+      repo_url        = "git@git.${var.dns_zone}:chloe/homelab.git"
+      target_revision = "HEAD"
+      path            = "./applications/redis"
+      ref             = "config"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "redis"
+    }
+
+    sync_policy {
+      # automated {
+      #   prune       = true
+      #   self_heal   = true
+      #   allow_empty = true
+      # }
+      sync_options = [
+        "ServerSideApply=true",
+        "Validate=false",
+      ]
+      
+      retry {
+        limit = "3"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
 }
