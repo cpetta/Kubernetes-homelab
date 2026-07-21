bb046c724213279ad1ada265d7aafb69e23d3aeb
Chloe Graves <cpetta@pm.me>
2026-07-21 14:30:46 -0400

    Migrate proxmox load balancing to ArgoCD

--- infrastructure/terraform/032-external_proxmox.tf
+++ infrastructure/terraform/032-external_proxmox.tf
@@ -209,3 +209,45 @@ resource "kubernetes_manifest" "load_ballance_pm_ingress" {
     }
   }
 }
+
+resource "argocd_application" "proxmox" {
+  metadata {
+    name      = "proxmox"
+    namespace = kubernetes_namespace_v1.argo.id
+  }
+
+  spec {
+    source {
+      repo_url        = "git@git.${var.dns_zone}:chloe/homelab.git"
+      target_revision = "HEAD"
+      path            = "./applications/proxmox"
+      ref             = "config"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "proxmox"
+    }
+
+    sync_policy {
+      # automated {
+      #   prune       = true
+      #   self_heal   = true
+      #   allow_empty = true
+      # }
+      sync_options = [
+        "ServerSideApply=true",
+        "Validate=false",
+      ]
+      
+      retry {
+        limit = "3"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
+}
