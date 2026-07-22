8f225286e35de145e0d5bffff6ae3041d14b5843
Chloe Graves <cpetta@pm.me>
2026-07-22 10:51:33 -0400

    Migrate longhorn oauth2-proxy to ArgoCD

--- infrastructure/terraform/100-longhorn.tf
+++ infrastructure/terraform/100-longhorn.tf
@@ -69,6 +69,60 @@ resource "argocd_application" "longhorn" {
   }
 }
 
+
+resource "argocd_application" "oauth2-proxy-longhorn" {
+  metadata {
+    name      = "longhorn-oauth2-proxy"
+    namespace = kubernetes_namespace_v1.argo.id
+  }
+
+  spec {
+    source {
+      repo_url = "https://oauth2-proxy.github.io/manifests"
+      chart = "oauth2-proxy"
+      target_revision = "10.4.3"
+      
+      helm {
+        release_name = "longhorn-oauth2-proxy"
+        value_files = ["$config/applications/oauth2-proxy-longhorn/values.yaml"]
+      }
+    }
+
+    source {
+      repo_url        = "git@git.${var.dns_zone}:chloe/homelab.git"
+      target_revision = "HEAD"
+      path            = "./applications/oauth2-proxy-longhorn"
+      ref             = "config"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "metrics"
+    }
+
+    sync_policy {
+      # automated {
+      #   prune       = true
+      #   self_heal   = true
+      #   allow_empty = true
+      # }
+      sync_options = [
+        "ServerSideApply=true",
+        "Validate=false",
+      ]
+      
+      retry {
+        limit = "3"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
+}
+
 #-------------------------------------------------------
 # HTTP Route (For recovory)
 #-------------------------------------------------------
