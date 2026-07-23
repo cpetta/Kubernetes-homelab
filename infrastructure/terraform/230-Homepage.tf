978d2dbc4541ad320a9083fe4c907def29f252c2
Chloe Graves <cpetta@pm.me>
2026-07-23 07:29:25 -0400

    Migrate homepage to ArgoCD

--- infrastructure/terraform/230-Homepage.tf
+++ infrastructure/terraform/230-Homepage.tf
@@ -565,3 +565,45 @@ resource "kubernetes_manifest" "homepage_httproute" {
     }
   }
 }
+
+resource "argocd_application" "homepage" {
+  metadata {
+    name      = "homepage"
+    namespace = kubernetes_namespace_v1.argo.id
+  }
+
+  spec {
+    source {
+      repo_url        = "git@git.${var.dns_zone}:chloe/homelab.git"
+      target_revision = "HEAD"
+      path            = "./applications/homepage"
+      ref             = "config"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "homepage"
+    }
+
+    sync_policy {
+      # automated {
+      #   prune       = true
+      #   self_heal   = true
+      #   allow_empty = true
+      # }
+      sync_options = [
+        "ServerSideApply=true",
+        "Validate=false",
+      ]
+      
+      retry {
+        limit = "3"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
+}
